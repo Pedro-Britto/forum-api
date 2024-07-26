3cba88ca7a76873d6c4eedca8301618e11ba5251
Pedro <79344913+Pedro-Britto@users.noreply.github.com>
2024-07-26 09:57:11 -0300

    feat: prisma

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async signupUser(
+    @Body() userData: { name?: string; email: string; password: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
